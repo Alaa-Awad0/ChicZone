7dd3fb481e598bde60d9e87a59ca9887d6c45161
Alaa-Awad0 <loly50987@gmail.com>
2025-03-11 10:49:53 +0200

    initial commit

--- src/app/pages/check-out/check-out.component.ts
+++ src/app/pages/check-out/check-out.component.ts
@@ -8,6 +8,7 @@ import {
 import { OrdersService } from '../../core/services/orders/orders.service';
 import { ToastrService } from 'ngx-toastr';
 import { CartService } from '../../core/services/cart/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -19,69 +20,70 @@ export class CheckOutComponent implements OnInit {
   private readonly formBuilder = inject(FormBuilder);
   private readonly ordersService = inject(OrdersService);
   private readonly cartService = inject(CartService);
-
   private readonly toastrService = inject(ToastrService);
+  private readonly router = inject(Router);
 
   isLoading: boolean = false;
-msgError: string = '';
-msgSuccess: string = '';
+  msgError: string = '';
+  msgSuccess: string = '';
 
-paymentMethod: string = '';
+  paymentMethod: string = '';
 
-checkOutForm!: FormGroup;
+  checkOutForm!: FormGroup;
 
-ngOnInit(): void {
-  this.checkOutForm = this.formBuilder.group({
-    details: [null, [Validators.required]],
-    phone: [
-      null,
-      [Validators.required, Validators.pattern(/^0[0125][0-9]{9}$/)],
-    ],
-    city: [null, [Validators.required]],
-  });
-}
-
-setPaymentMethod(method: string): void {
-  this.paymentMethod = method;
-}
-
-submitForm(): void {
-  if (this.checkOutForm.valid) {
-    if (this.paymentMethod === 'card') {
-      this.payWithCard();
-    } else if (this.paymentMethod === 'cash') {
-      this.payWithCash();
-    }
-  } else {
-    this.checkOutForm.markAllAsTouched();
+  ngOnInit(): void {
+    this.checkOutForm = this.formBuilder.group({
+      details: [null, [Validators.required]],
+      phone: [
+        null,
+        [Validators.required, Validators.pattern(/^0[0125][0-9]{9}$/)],
+      ],
+      city: [null, [Validators.required]],
+    });
   }
-}
 
-payWithCard(): void {
-  this.ordersService.checkOutCardPayment(this.checkOutForm.value).subscribe({
-    next: (res) => {
-      if (res.status === 'success') {
-        this.cartService.cartNum.set(0);
-        open(res.session.url, '_self');
-      }
-    },
-    error: (err) => {
-      this.toastrService.error(err.message, 'Error');
-    },
-  });
-}
+  setPaymentMethod(method: string): void {
+    this.paymentMethod = method;
+  }
 
-payWithCash(): void {
-  this.ordersService.createCashOrder(this.checkOutForm.value).subscribe({
-    next: (res) => {
-      if (res.status === 'success') {
-        this.cartService.cartNum.set(0);
-        this.toastrService.success(res.message, 'Success');
+  submitForm(): void {
+    if (this.checkOutForm.valid) {
+      if (this.paymentMethod === 'card') {
+        this.payWithCard();
+      } else if (this.paymentMethod === 'cash') {
+        this.payWithCash();
       }
-    },
-    error: (err) => {
-      this.toastrService.error(err.message, 'Error');
-    },
-  });
-}
+    } else {
+      this.checkOutForm.markAllAsTouched();
+    }
+  }
+
+  payWithCard(): void {
+    this.ordersService.checkOutCardPayment(this.checkOutForm.value).subscribe({
+      next: (res) => {
+        if (res.status === 'success') {
+          this.cartService.cartNum.set(0);
+          open(res.session.url, '_self');
+        }
+      },
+      error: (err) => {
+        this.toastrService.error(err.message, 'Error');
+      },
+    });
+  }
+
+  payWithCash(): void {
+    this.ordersService.createCashOrder(this.checkOutForm.value).subscribe({
+      next: (res) => {
+        if (res.status === 'success') {
+          this.cartService.cartNum.set(0);
+          this.toastrService.success(res.message, 'Success');
+          this.router.navigate(['/allorders']);
+        }
+      },
+      error: (err) => {
+        this.toastrService.error(err.message, 'Error');
+      },
+    });
+  }
 }
